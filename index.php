9f178cb8c45f6fc65b42d4156b9039b077160da7
WilliamsChazal <w.chazal@codeur.online>
2022-01-05 18:09:46 +0100

    appel depuis bbd nom & taux

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
+<?php
 
+        require_once('db-connect.php');
+        $sql ='SELECT * FROM  `currency` ';       
+        $query = $db->prepare($sql);
+        $query ->execute();
+        $result = $query->fetch();
+
+?>
   <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,13 +17,39 @@
     <title>Document</title>
 </head>
 <body>
+
+<section>
+    <div>
+        <span><h1>Taux de change</h1></span>
+        <span>Date et heure</span>
+    </div>
+    <div>
+    <span class="span1">
+            <span>taux actuel</span>
+            <span>taux max</span>
+            <span>taux mini</span>
+        </span>
+    </div>
+    <div class='div1'>
+
+        <span><?=$result['currencyName']?></span>
+        <span class='span2'>
+            <p><?=$result['currencyRate']?></p>
+            <p>100</p>
+            <p>100</p>
+        </span>
+    
+
+  
+    </div>
+
+</section>
+
     <?php
 
 /* https://api.ibanfirst.com/PublicAPI/Rate/USDEUR/ */
     $json = file_get_contents('http://localhost/test_api/ibanFirst.php');
     $obj = json_decode($json, true);
-
-    
     ?>
 <DIV><?php echo $obj{'rate'}{'instrument'};?></DIV>
 <DIV><?php echo $obj{'rate'}{'rate'};?></DIV>
--- main.js
+++ main.js
@@ -4,13 +4,11 @@ const BASE_ENDPOINT ="https://api.ibanfirst.com/PublicAPI";
 const url = `${BASE_ENDPOINT}/Rate/${currency1}${currency2}/`;
 
 async function RateChange(){
-    res = await fetch(url, {
-        mode :'no-cors',
-    });
-    JSON = await res.JSON();
-    JSON.status == 200;
-    JSON.rate.rate
-    document.getElementById('rateChange').innerHTML = JSON.rate.rate; 
+    res = await fetch(url);
+    json = await res.json();
+    json.status == 200;
+    json.rate.rate
+    document.getElementById('rateChange').innerHTML = json.rate.rate; 
     
 }
 
