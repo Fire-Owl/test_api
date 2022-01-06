27e370fb8263f8d1b8b12f45c3d34526ccf9fe77
WilliamsChazal <w.chazal@codeur.online>
2022-01-06 13:59:52 +0100

    ajout heure et date en js

--- index.php
+++ index.php
@@ -3,32 +3,28 @@
 $date = date("Y-m-d");
 
         require_once('db-connect.php');
+
         $viewProject = $db->query('SELECT * FROM currency  ORDER BY currencyID DESC LIMIT 1');
-        while ($viewProjectRespons = $viewProject->fetch()){
-        
-       $taux_actuel = $viewProjectRespons['currencyRate'];
-       $taux_name = $viewProjectRespons['currencyName'];
-        }
-
-
+        while ($viewProjectRespons = $viewProject->fetch()){ 
+                $taux_actuel = $viewProjectRespons['currencyRate'];
+                $taux_name = $viewProjectRespons['currencyName'];
+                }
             $viewProject= $db->prepare('SELECT * FROM currency WHERE currencyDate LIKE :date ORDER BY  currencyRate DESC LIMIT 1 ');
-            $viewProject->execute(array(
-            'date' => $date
-            ));
+                $viewProject->execute(array(
+                    'date' => $date
+                    )
+                );
 
         while ($viewProjectRespons = $viewProject->fetch()){
-        
-       $taux_max = $viewProjectRespons['currencyRate'];
-        
-    }   
-    $viewProject= $db->prepare('SELECT * FROM currency WHERE currencyDate LIKE :date ORDER BY  currencyRate ASC LIMIT 1 ');
-    $viewProject->execute(array(
-    'date' => $date
-    ));
+            $taux_max = $viewProjectRespons['currencyRate'];
+        }   
+        $viewProject= $db->prepare('SELECT * FROM currency WHERE currencyDate LIKE :date ORDER BY  currencyRate ASC LIMIT 1 ');
+        $viewProject->execute(array(
+                    'date' => $date
+                    )
+                );
         while ($viewProjectRespons = $viewProject->fetch()){
-        
-       $taux_mini = $viewProjectRespons['currencyRate'];
-        
+       $taux_mini = $viewProjectRespons['currencyRate'];  
     }   
    
 ?>
@@ -46,7 +42,14 @@ $date = date("Y-m-d");
     <section>
         <div>
             <span><h1>Taux de change</h1></span>
-            <span>Date et heure</span>
+            <div id="digital-clock__time">
+            <span id="digital-clock__time--hours"></span>
+            <span>:</span>
+            <span id="digital-clock__time--minutes"></span>
+            <span>:</span>
+            <span id="digital-clock__time--seconds"></span>
+            <span id="digital-clock__date--day"></span>
+     </div>
         </div>
         <div>
             <span class="span1">
@@ -65,15 +68,18 @@ $date = date("Y-m-d");
         </div>
     </section>
 
-    <?php
 
-/* https://api.ibanfirst.com/PublicAPI/Rate/USDEUR/ */
-    $json = file_get_contents('http://localhost/test_api/ibanFirst.php');
+    <?php
+/* ibanFirst.php */
+
+    $json = file_get_contents('http://localhost/test_api/https://api.ibanfirst.com/PublicAPI/Rate/USDEUR/');
     $obj = json_decode($json, true);
     ?>
 <DIV><?php echo $obj{'rate'}{'instrument'};?></DIV>
 <DIV><?php echo $obj{'rate'}{'rate'};?></DIV>
 <DIV><?php echo $obj{'date'}?></DIV>
 
+
+<script src="main.js"></script>
 </body>
 </html>
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const currency1 = "USD";
+/* const currency1 = "USD";
 const currency2 = "EUR";
 const BASE_ENDPOINT ="https://api.ibanfirst.com/PublicAPI";
 const url = `${BASE_ENDPOINT}/Rate/${currency1}${currency2}/`;
@@ -12,4 +12,29 @@ async function RateChange(){
     
 }
 
-window.onload = RateChange();
+window.onload = RateChange(); */
+
+const displayHours = document.getElementById('digital-clock__time--hours');
+const displayMinutes = document.getElementById('digital-clock__time--minutes');
+const displaySeconds = document.getElementById('digital-clock__time--seconds');
+const displayDay = document.getElementById('digital-clock__date--day');
+
+const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+
+setInterval(()=>{
+
+    let date = new Date(); 
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let day = date.toLocaleDateString('fr-FR',options);
+      seconds = seconds<10?"0"+seconds:seconds;
+      minutes = minutes<10?"0"+minutes:minutes;
+      hours = hours<10?"0"+hours:hours;
+
+    displayHours.innerHTML=hours;
+    displayMinutes.innerHTML=minutes;
+    displaySeconds.innerHTML =seconds;
+    displayDay.innerHTML = day;
+
+},1000);
